494a73120f621aaa234563f6f688e03230cda776
niketarachhadia <niketa.rachhadia@gmail.com>
2016-10-02 01:13:23 -0500

    deployment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                             'mongodb://localhost/xml2json' :
+                            'mongodb://test:test@ds047666.mlab.com:47666/xml2json-mlab' :
                             'mongodb://localhost/xml2json-dev');
 exports.PORT = process.env.PORT || 8089;
